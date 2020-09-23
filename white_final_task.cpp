1ab1e14810efccb070a801999bc06c4d0d5ecf18
muhtimur <muhamedishin.timur@gmail.com>
2020-09-23 21:43:58 +0700

    Add white belt final task

--- white_final_task.cpp
+++ white_final_task.cpp
@@ -13,272 +13,272 @@ using namespace std;
 
 class Date {
 public:
-    Date() {
-        this->year = 0;
-        this->month = 0;
-        this->day = 0;
-    }
+	Date() {
+		this->year = 0;
+		this->month = 0;
+		this->day = 0;
+	}
 
-    Date(const int& input_year, const int& input_month, const int& input_day) {
-        this->year = input_year;
-        this->month = input_month;
-        this->day = input_day;
-    }
+	Date(const int& input_year, const int& input_month, const int& input_day) {
+		this->year = input_year;
+		this->month = input_month;
+		this->day = input_day;
+	}
 
-    int GetYear() const {
-        return this->year;
-    }
-    
-    int GetMonth() const {
-        return this->month;
-    }
+	int GetYear() const {
+		return this->year;
+	}
 
-    int GetDay() const {
-        return this->day;
-    }
+	int GetMonth() const {
+		return this->month;
+	}
+
+	int GetDay() const {
+		return this->day;
+	}
 
 private:
-    int year;
-    int month;
-    int day;
+	int year;
+	int month;
+	int day;
 };
 
 bool operator<(const Date& lhs, const Date& rhs) {
-    if (lhs.GetYear() > rhs.GetYear())
-        return false;
-    else if (lhs.GetYear() < rhs.GetYear())
-        return true;
-    else if (lhs.GetMonth() > rhs.GetMonth())
-        return false;
-    else if (lhs.GetMonth() < rhs.GetMonth())
-        return true;
-    else if (lhs.GetDay() > rhs.GetDay())
-        return false;
-    else if (lhs.GetDay() < rhs.GetDay())
-        return true;
-    else // same dates
-        return false;
+	if (lhs.GetYear() > rhs.GetYear())
+		return false;
+	else if (lhs.GetYear() < rhs.GetYear())
+		return true;
+	else if (lhs.GetMonth() > rhs.GetMonth())
+		return false;
+	else if (lhs.GetMonth() < rhs.GetMonth())
+		return true;
+	else if (lhs.GetDay() > rhs.GetDay())
+		return false;
+	else if (lhs.GetDay() < rhs.GetDay())
+		return true;
+	else // same dates
+		return false;
 }
 
 bool operator==(const Date& lhs, const Date& rhs) {
-    return (lhs.GetDay() == rhs.GetDay()) && (lhs.GetMonth() == rhs.GetMonth()) && (lhs.GetYear() == rhs.GetYear());
+	return (lhs.GetDay() == rhs.GetDay()) && (lhs.GetMonth() == rhs.GetMonth()) && (lhs.GetYear() == rhs.GetYear());
 }
 
 class Event {
 public:
-    Event(const string& input_name) {
-        this->name = input_name;
-    }
+	Event(const string& input_name) {
+		this->name = input_name;
+	}
 
-    string GetName() const {
-        return this->name;
-    }
+	string GetName() const {
+		return this->name;
+	}
 
 private:
-    string name;
+	string name;
 };
 
 bool operator<(const Event& lhs, const Event& rhs) {
-    if (lhs.GetName() < rhs.GetName())
-        return true;
-    return false;
+	if (lhs.GetName() < rhs.GetName())
+		return true;
+	return false;
 }
 
 class Database {
 public:
-    void AddEvent(const Date& date, const string& event) {
-        this->dataset[date].insert(event);
-    }
+	void AddEvent(const Date& date, const string& event) {
+		this->dataset[date].insert(event);
+	}
 
-    bool DeleteEvent(const Date& date, const string& event) {
-        if(this->dataset.find(date) != this->dataset.end())
-            if (this->dataset.at(date).find(event) != this->dataset.at(date).end())
-                return this->dataset.at(date).erase(event);
+	bool DeleteEvent(const Date& date, const string& event) {
+		if (this->dataset.find(date) != this->dataset.end())
+			if (this->dataset.at(date).find(event) != this->dataset.at(date).end())
+				return this->dataset.at(date).erase(event);
 
-        return false;
-    }
+		return false;
+	}
 
-    int  DeleteDate(const Date& date) {
-        if (this->dataset.find(date) != this->dataset.end()) {
-            int numberOfDeletedItems = this->dataset.at(date).size();
-            this->dataset.at(date).clear();
-            return numberOfDeletedItems;
-        } 
+	int  DeleteDate(const Date& date) {
+		if (this->dataset.find(date) != this->dataset.end()) {
+			int numberOfDeletedItems = this->dataset.at(date).size();
+			this->dataset.at(date).clear();
+			return numberOfDeletedItems;
+		}
 
-        return 0;        
-    }
+		return 0;
+	}
 
-    set<string> Find(const Date& date) const {
-        set<string> result;
+	set<string> Find(const Date& date) const {
+		set<string> result;
 
-        if(this->dataset.find(date) != this->dataset.end())
-            for (auto it : this->dataset.at(date))
-                result.insert(it.GetName());        
+		if (this->dataset.find(date) != this->dataset.end())
+			for (auto it : this->dataset.at(date))
+				result.insert(it.GetName());
 
-        return result;
-    }
+		return result;
+	}
 
-    void Print() const {
-        for (auto date : this->dataset) {
-            for (auto event : date.second) {
-                std::cout << std::setw(4) << std::setfill('0') << date.first.GetYear() << "-";
-                std::cout << std::setw(2) << std::setfill('0') << date.first.GetMonth() << "-";
-                std::cout << std::setw(2) << std::setfill('0') << date.first.GetDay() << " ";
-                std::cout << event.GetName() << endl;
-            }
-        }
-    }
+	void Print() const {
+		for (auto date : this->dataset) {
+			for (auto event : date.second) {
+				std::cout << std::setw(4) << std::setfill('0') << date.first.GetYear() << "-";
+				std::cout << std::setw(2) << std::setfill('0') << date.first.GetMonth() << "-";
+				std::cout << std::setw(2) << std::setfill('0') << date.first.GetDay() << " ";
+				std::cout << event.GetName() << endl;
+			}
+		}
+	}
 
 private:
-    map<Date, set<Event>> dataset;
+	map<Date, set<Event>> dataset;
 };
 
 void EnsureNextSymbolAndSkip(stringstream& stream) {
-    if (stream.peek() != '-') {
-        stringstream ss;
-        ss << "expected - , but has: " << char(stream.peek());
-        throw runtime_error(ss.str());
-    }
-    stream.ignore(1);
-    if ((stream.peek() < '0' || stream.peek() > '9') && stream.peek() != '+' && stream.peek() != '-') {
-        stringstream ss;
-        ss << "expected digit, but has: " << char(stream.peek());
-        throw runtime_error(ss.str());
-    }
+	if (stream.peek() != '-') {
+		stringstream ss;
+		ss << "expected - , but has: " << char(stream.peek());
+		throw runtime_error(ss.str());
+	}
+	stream.ignore(1);
+	if ((stream.peek() < '0' || stream.peek() > '9') && stream.peek() != '+' && stream.peek() != '-') {
+		stringstream ss;
+		ss << "expected digit, but has: " << char(stream.peek());
+		throw runtime_error(ss.str());
+	}
 }
 
 Date StringToDate(const string& s) {
-    int year, month, day;
-    stringstream stream(s);
-    Date date;
+	int year, month, day;
+	stringstream stream(s);
+	Date date;
 
-    // check first symbol
-    if (stream.peek() != '-' && stream.peek() != '+' && (stream.peek() < '0' || stream.peek() > '9')) {
-        stringstream ss;
-        ss << "Wrong date format: " << s;
-        throw runtime_error(ss.str());
-    }
+	// check first symbol
+	if (stream.peek() != '-' && stream.peek() != '+' && (stream.peek() < '0' || stream.peek() > '9')) {
+		stringstream ss;
+		ss << "Wrong date format: " << s;
+		throw runtime_error(ss.str());
+	}
 
-    try {
-        stream >> year;
-        EnsureNextSymbolAndSkip(stream);
-        stream >> month;
-        EnsureNextSymbolAndSkip(stream);
-        stream >> day;
-        
-    }
-    catch (...) {
-        stringstream ss;
-        ss << "Wrong date format: " << s;
-        throw runtime_error(ss.str());
-    }
+	try {
+		stream >> year;
+		EnsureNextSymbolAndSkip(stream);
+		stream >> month;
+		EnsureNextSymbolAndSkip(stream);
+		stream >> day;
 
-    // check last symbol
-    if (stream.peek() != -1) {
-        stringstream ss;
-        ss << "Wrong date format: " << s;
-        throw runtime_error(ss.str());
-    }
+	}
+	catch (...) {
+		stringstream ss;
+		ss << "Wrong date format: " << s;
+		throw runtime_error(ss.str());
+	}
 
-    // checking whether ranges are correct
-    if (month < 1 || month > 12) {
-        stringstream ss;
-        ss << "Month value is invalid: " << month;
-        throw runtime_error(ss.str());
-    }
-    else if (day < 1 || day > 31) {
-        stringstream ss;
-        ss << "Day value is invalid: " << day;
-        throw runtime_error(ss.str());
-    }
+	// check last symbol
+	if (stream.peek() != -1) {
+		stringstream ss;
+		ss << "Wrong date format: " << s;
+		throw runtime_error(ss.str());
+	}
 
-    return Date(year, month, day);;
+	// checking whether ranges are correct
+	if (month < 1 || month > 12) {
+		stringstream ss;
+		ss << "Month value is invalid: " << month;
+		throw runtime_error(ss.str());
+	}
+	else if (day < 1 || day > 31) {
+		stringstream ss;
+		ss << "Day value is invalid: " << day;
+		throw runtime_error(ss.str());
+	}
+
+	return Date(year, month, day);;
 }
 
 
 int main() {
-    Database db;
-    string command;
+	Database db;
+	string command;
 
-    while (getline(cin, command)) {
-        stringstream stream(command);
-        string command_name;
-        stream >> command_name;
+	while (getline(cin, command)) {
+		stringstream stream(command);
+		string command_name;
+		stream >> command_name;
 
-        if (command_name == "Add") {
-            Date date;
-            string date_string, name;
-            
-            stream >> date_string;
-            stream >> name;
+		if (command_name == "Add") {
+			Date date;
+			string date_string, name;
 
-            try {
-                date = StringToDate(date_string);
-            }
-            catch (exception& ex) {
-                std::cout << ex.what() << endl;
-                continue;
-            }
-            db.AddEvent(date, name);
-        }
-        else if (command_name == "Del") {
+			stream >> date_string;
+			stream >> name;
 
-            string date_string, name;
-            Date date;
+			try {
+				date = StringToDate(date_string);
+			}
+			catch (exception& ex) {
+				std::cout << ex.what() << endl;
+				continue;
+			}
+			db.AddEvent(date, name);
+		}
+		else if (command_name == "Del") {
 
-            stream >> date_string;
-            stream >> name;
+			string date_string, name;
+			Date date;
 
-            try {
-                date = StringToDate(date_string);
-            }
-            catch (exception& ex) {
-                std::cout << ex.what() << endl;
-                continue;
-            }
+			stream >> date_string;
+			stream >> name;
 
-            if (name == "") {
-                int res = db.DeleteDate(date);
-                std::cout << "Deleted " << res << " events" << endl;
-            }
-            else {
-                if (db.DeleteEvent(date, name))
-                    std::cout << "Deleted successfully" << endl;
-                else
-                    std::cout << "Event not found" << endl;
-            }        
-        }
-        else if (command_name == "Find") {
+			try {
+				date = StringToDate(date_string);
+			}
+			catch (exception& ex) {
+				std::cout << ex.what() << endl;
+				continue;
+			}
 
-            string date_string, name;
-            Date date;
+			if (name == "") {
+				int res = db.DeleteDate(date);
+				std::cout << "Deleted " << res << " events" << endl;
+			}
+			else {
+				if (db.DeleteEvent(date, name))
+					std::cout << "Deleted successfully" << endl;
+				else
+					std::cout << "Event not found" << endl;
+			}
+		}
+		else if (command_name == "Find") {
 
-            stream >> date_string;
-            stream >> name;
+			string date_string, name;
+			Date date;
 
-            try {
-                date = StringToDate(date_string);
-            }
-            catch (exception& ex) {
-                std::cout << ex.what() << endl;
-                continue;
-            }
+			stream >> date_string;
+			stream >> name;
 
-            set<string> res = db.Find(date);
+			try {
+				date = StringToDate(date_string);
+			}
+			catch (exception& ex) {
+				std::cout << ex.what() << endl;
+				continue;
+			}
 
-            if (!res.empty()) {
-                for (string it : res) {
-                    cout << it << endl;
-                }
-            }
-        }
-        else if (command_name == "Print") {
-            db.Print();
-        }
-        else if(command_name != "") {
-            std::cout << "Unknown command: " << command_name << endl;
-        }
-    }
+			set<string> res = db.Find(date);
 
-    return 0;
+			if (!res.empty()) {
+				for (string it : res) {
+					cout << it << endl;
+				}
+			}
+		}
+		else if (command_name == "Print") {
+			db.Print();
+		}
+		else if (command_name != "") {
+			std::cout << "Unknown command: " << command_name << endl;
+		}
+	}
+
+	return 0;
 }
